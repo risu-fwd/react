2d48677fcb7fb8aacc756af52172b9447df8b414
risu-fwd <fwd@ymail.ne.jp>
2024-01-08 17:16:24 +0900

    #43 Firestoreからのデータの取得と表示

--- 03_shin-code/blog-with-react-and-firebase/src/components/Home.js
+++ 03_shin-code/blog-with-react-and-firebase/src/components/Home.js
@@ -1,40 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
 
 const Home = () => {
+  const [postList, setPostList] = useState([]);
+
   useEffect(() => {
     // #42 追記分
     const getPosts = async () => {
       const data = await getDocs(collection(db, "posts"));
-      console.log(data);
-      console.log(data.docs);
-      console.log(data.docs.map((doc) => ({ doc })));
-      console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      // console.log(data);
+      // console.log(data.docs);
+      // console.log(data.docs.map((doc) => ({ doc })));
+      // console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
     getPosts();
   }, []);
 
   return (
     <div className="homePage">
-      <div className="postContents">
-        <div className="postHeader">
-          <h1>タイトル</h1>
-        </div>
-        <div className="postTextContainer">
-          React学習中
-          <br />
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia
-          facilis nam officiis ad reiciendis distinctio laudantium doloremque
-          quae enim fugiat. Alias accusantium rerum, exercitationem ex cum quae
-          voluptatum minima facilis.
-        </div>
-        <div className="nameAndDeleteButton">
-          <h3>@Shincode</h3>
-          <button>削除</button>
-        </div>
-      </div>
+      {postList.map((post) => {
+        return (
+          // <div className="postContents">
+          <div className="postContents" key={post.id}>
+            <div className="postHeader">
+              <h1>{post.title}</h1>
+            </div>
+            <div className="postTextContainer">{post.postsText}</div>
+            <div className="nameAndDeleteButton">
+              <h3>@{post.author.username}</h3>
+              <button>削除</button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
