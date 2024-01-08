5381b5effc069e60643d36972808b2d766176c4e
risu-fwd <fwd@ymail.ne.jp>
2024-01-08 16:19:45 +0900

    Cloud Firestoreからデータの取得

--- 03_shin-code/blog-with-react-and-firebase/src/components/Home.js
+++ 03_shin-code/blog-with-react-and-firebase/src/components/Home.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./Home.css";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 
 const Home = () => {
-  return <div>Home</div>;
+  useEffect(() => {
+    // #42 追記分
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+      console.log(data);
+      console.log(data.docs);
+      console.log(data.docs.map((doc) => ({ doc })));
+      console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  }, []);
+
+  return (
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+        <div className="postTextContainer">
+          React学習中
+          <br />
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia
+          facilis nam officiis ad reiciendis distinctio laudantium doloremque
+          quae enim fugiat. Alias accusantium rerum, exercitationem ex cum quae
+          voluptatum minima facilis.
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3>@Shincode</h3>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
